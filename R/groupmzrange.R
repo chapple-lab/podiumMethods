4af7b11077daa2853f6d6355337459b899ad15bb
Cole Wunderlich <wunderli@purdue.edu>
2017-06-29 14:04:25 -0400

    Updated groupmzrange to accomodate different labeling patterns an masses
    Working on converting pairing method to handle any combination of labelling pattern and label mass.  Need to make sure these options are accessable to the user

--- R/groupmzrange.R
+++ R/groupmzrange.R
@@ -1,9 +1,12 @@
+##DEPRECATED::calculates range using ppm of 12C peak, not 13C peak, may want to change
+##changed to calculate based on the 13C peak
+##For ring-labeled phe use mLabel = 1.0033548 and nLabel = 6
 groupmzrange <-
-function(groups,mzppm=10,mzabs=0.005,nC13=6) {
-  C13mzdif<-1.0033548   ##mass of 13C
-  mztor<-(groups$mzmed+C13mzdif*nC13)*mzppm*1e-6+mzabs  ##calculate M/z tolerance
-  uppermz<-groups$mzmed+C13mzdif*nC13+mztor
-  lowermz<-groups$mzmed+C13mzdif*nC13-mztor
+function(groups,mzppm=10,mzabs=0.005,nLabel=6,mLabel=1.0033548) {
+  ##mass of 13C C13mzdif<-1.0033548   
+  mztor<-(groups$mzmed+mLabel*nLabel)*mzppm*1e-6+mzabs  ##calculate M/z tolerance
+  uppermz<-groups$mzmed+mLabel*nLabel+mztor
+  lowermz<-groups$mzmed+mLabel*nLabel-mztor
   allpeaks<-cbind(groups,lowermz=lowermz,uppermz=uppermz) ##add calculated upper and lower ranges as columns to allpeaks dataframe
-  #   write.csv(allpeaks,paste("allpeaksC",nC13,".csv",sep=""),row.names=F)
+  #   write.csv(allpeaks,paste("allpeaksC",nLabel,".csv",sep=""),row.names=F)
 }
